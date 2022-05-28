9a42fb4a726b7b852dea9ecb68183c3041b67270
Lellolxs <nagyonlol69@gmail.com>
2022-05-28 22:21:43 +0200

    Taking pictures

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,23 +1,33 @@
 import numpy as np
 import cv2 as cv
+COLOR = (255,0,0)
+STROKE = 3
 
 face_cascade = cv.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml')
+CAP = cv.VideoCapture(0)
+CAP.set(3, 320)
+CAP.set(4, 240)
 
-cap = cv.VideoCapture(0)
-cap.set(3, 480)
-cap.set(4, 480)
+def registerMember():
+    while True:
+        ret, frame = CAP.read()
+        frameGreyscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(frameGreyscale, 1.1, 4)
 
-color = (255,0,0)
-stroke = 3
-while True:
-    ret, frame = cap.read()
-    frameGreyscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(frameGreyscale, 1.2, 4)
-    for (x,y,w,h) in faces:
-        cv.rectangle(frame, (x,y), (x+w,y+h), color, stroke )
-    cv.imshow('frame', frame)
-    if cv.waitKey(20) & 0xFF == ord('q'):
-        break
+        print(faces, len(faces))
 
-cap.release()
-cv.destroyAllWindows()
+        for (x,y,w,h) in faces:
+            cv.rectangle(frame, (x,y), (x+w,y+h), COLOR, STROKE )
+
+        resized = cv.resize(frame, (640, 480))
+
+        cv.imshow('frame', resized)
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            if len(faces) > 0:
+                for i in range(5):
+                    cv.imwrite(f'{i}.png', frame)
+                break
+    CAP.release()
+    cv.destroyAllWindows()
+
+registerMember()
