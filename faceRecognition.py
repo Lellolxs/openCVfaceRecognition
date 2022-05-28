ea07949de354359a50cf10cfbe8bd512872990f2
Lellolxs <nagyonlol69@gmail.com>
2022-05-28 23:49:39 +0200

    Images taken will no longer have a blue rectangle covering them.

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,7 +1,8 @@
+from tkinter import Frame
 import numpy as np
 import cv2 as cv
 COLOR = (255,0,0)
-STROKE = 3
+STROKE = 2
 
 face_cascade = cv.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml')
 CAP = cv.VideoCapture(0)
@@ -11,8 +12,15 @@ CAP.set(4, 240)
 def registerMember():
     while True:
         ret, frame = CAP.read()
+
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            if len(faces) > 0:
+                for i in range(5):
+                    cv.imwrite(f'registeredMembers/{i}.png', frame)
+                break
+
         frameGreyscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(frameGreyscale, 1.1, 4)
+        faces = face_cascade.detectMultiScale(frameGreyscale, 1.2, 4)
 
         print(faces, len(faces))
 
@@ -22,11 +30,6 @@ def registerMember():
         resized = cv.resize(frame, (640, 480))
 
         cv.imshow('frame', resized)
-        if cv.waitKey(20) & 0xFF == ord('q'):
-            if len(faces) > 0:
-                for i in range(5):
-                    cv.imwrite(f'{i}.png', frame)
-                break
     CAP.release()
     cv.destroyAllWindows()
 
